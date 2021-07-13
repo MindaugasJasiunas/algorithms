35babcb76834c3a3f07632ce03cd23623586df60
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-07-13 16:42:49 +0300

    get tree depth

---
+++ TreeDepth/src/main/java/Tree.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tree {
+    /**
+     * Method that searches tree depth using recursion
+     * @param branch
+     * @return int value of tree depth
+     */
+    public int getDepth(TreeBranch branch){
+        if(branch == null){
+            return 0;
+        }
+        int deepest=0;
+
+        if(branch.getBranches() != null){
+            for(TreeBranch treeBranch : branch.getBranches()){
+                deepest = Math.max(deepest, getDepth(treeBranch));
+            }
+        }
+        return deepest+1; //deepest + current branch
+    }
+
+}
+
+
+class TreeBranch {
+    private List<TreeBranch> branches;
+
+    public TreeBranch(){
+        branches=new ArrayList<>();
+    }
+
+    public void setBranch(TreeBranch branch){
+        if(branch!=null){
+            branches.add(branch);
+        }
+    }
+    public List<TreeBranch> getBranches(){
+        return branches;
+    }
+
+}
---
+++ TreeDepth/src/test/java/TreeTest.java
@@ -0,0 +1,72 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TreeTest {
+    private final Tree tree = new Tree();
+
+    @DisplayName("getDepth() - depth is 5")
+    @Test
+    void getDepth() {
+        /*
+         * Testing tree visual representation with branch numbers:
+         *
+         *     (0)
+         *    /   \
+         *  (2)   (10)
+         *  /    / | \
+         *(1)  (8)(6)(9)
+         *     /  /\
+         *   (7)(4)(5)
+         *       |
+         *      (3)
+         *
+         */
+
+        //kuriam sakas ir sakas sakose
+        TreeBranch branch1=new TreeBranch();
+        TreeBranch branch2=new TreeBranch();
+        branch2.setBranch(branch1);
+
+        TreeBranch branch3=new TreeBranch();
+        TreeBranch branch4=new TreeBranch();
+        branch4.setBranch(branch3);
+
+        TreeBranch branch5=new TreeBranch();
+        TreeBranch branch6=new TreeBranch();
+        branch6.setBranch(branch5);
+        branch6.setBranch(branch4);
+
+        TreeBranch branch7=new TreeBranch();
+        TreeBranch branch8=new TreeBranch();
+        branch8.setBranch(branch7);
+
+        TreeBranch branch9=new TreeBranch();
+        TreeBranch branch10=new TreeBranch();
+        branch10.setBranch(branch9);
+        branch10.setBranch(branch6);
+        branch10.setBranch(branch8);
+
+        //root branch
+        TreeBranch branch0=new TreeBranch();
+        branch0.setBranch(branch2);
+        branch0.setBranch(branch10);
+
+        assertEquals(5, tree.getDepth(branch0));
+    }
+
+    @DisplayName("getDepth() with null value - depth is 0")
+    @Test
+    void getDepth_nullValue() {
+        assertEquals(0, tree.getDepth(null));
+    }
+
+    @DisplayName("getDepth() with 1 branch - depth is 1")
+    @Test
+    void getDepth_oneBranch() {
+        TreeBranch branch1=new TreeBranch();
+        assertEquals(1,tree.getDepth(branch1));
+    }
+}
