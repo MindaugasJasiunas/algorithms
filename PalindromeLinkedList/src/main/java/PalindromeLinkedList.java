dd138da9b29d89c5198be0a650ac7ad82b72477d
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-08-06 12:26:52 +0300

    Palindrome updated #5

--- PalindromeLinkedList/src/main/java/PalindromeLinkedList.java
+++ PalindromeLinkedList/src/main/java/PalindromeLinkedList.java
@@ -1,38 +1,39 @@
 public class PalindromeLinkedList {
 
-    boolean isPalindrome(ListNode head){
-        ListNode slow=head;
-        ListNode fast=head;
+    boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while(fast!=null && fast.next!=null){
-            fast=fast.next.next; //double the speed
-            slow=slow.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next; //double the speed
+            slow = slow.next;
         }
-        slow=reversed(slow);
-        fast=head;
-        while(slow!=null){
-            if(slow.value != fast.value){
+        slow = reversed(slow);
+        fast = head;
+        while (slow != null) {
+            if (slow.value != fast.value) {
                 return false;
             }
-            slow=slow.next;
-            fast=fast.next;
+            slow = slow.next;
+            fast = fast.next;
         }
         return true;
     }
 
-    ListNode reversed(ListNode head){
-        ListNode prev=null;
-        while(head!=null){
-            ListNode next= head.next;
+    ListNode reversed(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
             //reverse pointer
-            head.next=prev;
-            prev=head;
-            head=next;
+            head.next = prev;
+            prev = head;
+            head = next;
         }
         return prev;
     }
 
 }
+
 class ListNode {
     int value;
     ListNode next;
