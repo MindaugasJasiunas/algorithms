73bfff6c2304f2d32b42bcf7b827efe7c1932716
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-08-06 12:26:30 +0300

    Roman numbers updated #4

--- RomanNumbersToArabic/src/main/java/RomanNumberToArabic.java
+++ RomanNumbersToArabic/src/main/java/RomanNumberToArabic.java
@@ -15,15 +15,15 @@ public class RomanNumberToArabic {
 //    Symbols V, L, and D cannot appear more than once consecutively.
 //    Do not subtract a number from one that is more than 10 times greater: I may only precede V and X, X may only precede L and C, and C may only precede D and M
 
-    public int convertToArabic(String romanText){
-        if(checkRomanFormat(romanText)){
-            int result=0;
-            int i=0;
-            while(romanText.length()>0){
-                for(Roman symbol : Roman.getSortedFromBiggest()){
-                    if(romanText.startsWith(symbol.name())){
-                        result+=symbol.getValue();
-                        romanText=romanText.substring(symbol.toString().length(), romanText.length());
+    public int convertToArabic(String romanText) {
+        if (checkRomanFormat(romanText)) {
+            int result = 0;
+            int i = 0;
+            while (romanText.length() > 0) {
+                for (Roman symbol : Roman.getSortedFromBiggest()) {
+                    if (romanText.startsWith(symbol.name())) {
+                        result += symbol.getValue();
+                        romanText = romanText.substring(symbol.toString().length(), romanText.length());
                         break;
                     }
                 }
@@ -33,13 +33,13 @@ public class RomanNumberToArabic {
         return -1;
     }
 
-    public String convertToRoman(int arabicNumber){
-        StringBuilder romanText=new StringBuilder();
-        int i=0;
-        while(arabicNumber>0){
-            for(Roman symbol : Roman.getSortedFromBiggest()){
-                while(arabicNumber/symbol.getValue()>=1){
-                    arabicNumber-=symbol.getValue();
+    public String convertToRoman(int arabicNumber) {
+        StringBuilder romanText = new StringBuilder();
+        int i = 0;
+        while (arabicNumber > 0) {
+            for (Roman symbol : Roman.getSortedFromBiggest()) {
+                while (arabicNumber / symbol.getValue() >= 1) {
+                    arabicNumber -= symbol.getValue();
                     romanText.append(symbol);
                 }
             }
@@ -47,38 +47,81 @@ public class RomanNumberToArabic {
         return romanText.toString();
     }
 
-    boolean checkRomanFormat(String input){
-        for(int i=0; i<input.length(); i++){
-            char character= input.charAt(i);
-            if(character!='I' && character!='V' && character!='X' && character!='L' && character!='C' && character!='D' && character!='M'){
+    boolean checkRomanFormat(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            char character = input.charAt(i);
+            if (character != 'I' && character != 'V' && character != 'X' && character != 'L' && character != 'C' && character != 'D' && character != 'M') {
                 return false;
             }
-
+            /*if(i+1 < input.length()){
+                // V , L or D more than once consecutively
+                if(character=='V' && input.charAt(i+1)=='V'){
+                    return false;
+                }
+                if(character=='L' && input.charAt(i+1)=='L'){
+                    return false;
+                }
+                if(character=='D' && input.charAt(i+1)=='D'){
+                    return false;
+                }
+                // dont subtract a number from one that is more than 10 times greater
+                if(input.charAt(i+1)=='V' && (character!='I' && character!='X' && character!='L' && character!='C' && character!='D' && character!='M')){
+                    return false;
+                }
+                if(input.charAt(i+1)=='X' && (character!='I' && character!='X' && character!='L' && character!='C' && character!='D' && character!='M')){
+                    return false;
+                }
+                if(input.charAt(i+1)=='L' && (character!='X' && character!='C' && character!='D' && character!='M')){
+                    return false;
+                }
+                if(input.charAt(i+1)=='C' && (character!='X' && character!='C' && character!='D' && character!='M')){
+                    return false;
+                }
+                if(input.charAt(i+1)=='D' && (character!='C' && character!='M')){
+                    return false;
+                }
+                if(input.charAt(i+1)=='M' && (character!='C' && character!='M')){
+                    return false;
+                }
+            }
+            if(i+3< input.length()){
+                //Do not repeat I, X, and C more than 3 times
+                if(character=='I' && input.charAt(i+1)=='I' && input.charAt(i+2)=='I' && input.charAt(i+3)=='I'){
+                    return false;
+                }
+                if(character=='X' && input.charAt(i+1)=='X' && input.charAt(i+2)=='X' && input.charAt(i+3)=='X'){
+                    return false;
+                }
+                if(character=='C' && input.charAt(i+1)=='C' && input.charAt(i+2)=='C' && input.charAt(i+3)=='C'){
+                    return false;
+                }
+            }*/
         }
         return true;
     }
 }
 
-enum Roman{
+enum Roman {
     I(1), IV(4), V(5), IX(9), X(10), XL(40), L(50), XC(90), C(100), CD(400), D(500), CM(900), M(1000);
 
-    Roman(int value){
-        this.value=value;
+    Roman(int value) {
+        this.value = value;
     }
+
     private int value;
 
     public int getValue() {
         return value;
     }
 
-    static List<Roman> getSortedFromBiggest(){
-        List<Roman> sorted=new ArrayList<>(Arrays.asList(Roman.values()));
+    static List<Roman> getSortedFromBiggest() {
+        List<Roman> sorted = new ArrayList<>(Arrays.asList(Roman.values()));
         sorted.sort((o1, o2) -> {
-            if(o1.value > o2.value){
+            if (o1.value > o2.value) {
                 return -1;
-            }else if(o1.value < o2.value){
+            } else if (o1.value < o2.value) {
                 return 1;
-            }else{
+            } else {
                 return 0;
             }
         });
--- RomanNumbersToArabic/src/test/java/RomanNumberToArabicTest.java
+++ RomanNumbersToArabic/src/test/java/RomanNumberToArabicTest.java
@@ -4,57 +4,82 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RomanNumberToArabicTest {
-    private final RomanNumberToArabic romanNumberToArabic=new RomanNumberToArabic();
+    private final RomanNumberToArabic romanNumberToArabic = new RomanNumberToArabic();
 
     @DisplayName("convertToArabic() XIX - 19")
     @Test
     void convertToArabic_19() {
-        int result= romanNumberToArabic.convertToArabic("XIX");
+        int result = romanNumberToArabic.convertToArabic("XIX");
         assertEquals(19, result);
     }
 
     @DisplayName("convertToArabic() MMXVIII - 2018")
     @Test
     void convertToArabic_2018() {
-        int result= romanNumberToArabic.convertToArabic("MMXVIII");
+        int result = romanNumberToArabic.convertToArabic("MMXVIII");
         assertEquals(2018, result);
     }
 
     @DisplayName("convertToArabic() MCMXCIX - 1999")
     @Test
     void convertToArabic_1999() {
-        int result= romanNumberToArabic.convertToArabic("MCMXCIX");
+        int result = romanNumberToArabic.convertToArabic("MCMXCIX");
         assertEquals(1999, result);
     }
-
 
 
     @DisplayName("convertToArabic() invalid input - NON ROMAN SYMBOLS - returns -1")
     @Test
     void convertToArabic_invalidInput() {
-        int result= romanNumberToArabic.convertToArabic("ABC");
+        int result = romanNumberToArabic.convertToArabic("ABC");
         assertEquals(-1, result);
     }
 
     @DisplayName("convertToRoman() 1999 - returns empty string")
     @Test
     void convertToRoman_1999() {
-        String result= romanNumberToArabic.convertToRoman(1999);
+        String result = romanNumberToArabic.convertToRoman(1999);
         assertEquals("MCMXCIX", result);
     }
 
     @DisplayName("convertToRoman() 2018 - returns empty string")
     @Test
     void convertToRoman_2018() {
-        String result= romanNumberToArabic.convertToRoman(2018);
+        String result = romanNumberToArabic.convertToRoman(2018);
         assertEquals("MMXVIII", result);
     }
 
     @DisplayName("convertToRoman() 0 - returns empty string")
     @Test
     void convertToRoman_zero() {
-        String result= romanNumberToArabic.convertToRoman(0);
+        String result = romanNumberToArabic.convertToRoman(0);
         assertTrue(result.isEmpty());
     }
+
+    /*@DisplayName("convertToArabic() invalid input - V & L & D repeats more than once consecutively - returns -1")
+    @Test
+    void test() {
+        assertEquals(-1, romanNumberToArabic.convertToArabic("VVI"));
+        assertEquals(-1, romanNumberToArabic.convertToArabic("LLI"));
+        assertEquals(-1, romanNumberToArabic.convertToArabic("DDI"));
+    }
+
+    @DisplayName("convertToArabic() invalid input - subtract a number from one that is more than 10 times greater - returns -1")
+    @Test
+    void convertToArabic_invalidInput_SubtractingInputFromOneThatIsMoreThan10TimesGreater() {
+        assertEquals(-1, romanNumberToArabic.convertToArabic("XM"));
+        assertEquals(-1, romanNumberToArabic.convertToArabic("IL"));
+        assertEquals(-1, romanNumberToArabic.convertToArabic("IC"));
+        assertEquals(-1, romanNumberToArabic.convertToArabic("XD"));
+    }
+
+    @DisplayName("convertToArabic() invalid input - I, X, C repeats more than 3 times - returns -1")
+    @Test
+    void convertToArabic_invalidInput_IXCRepeatsMoreThan3Times() {
+        assertEquals(-1, romanNumberToArabic.convertToArabic("IIII"));
+        assertEquals(-1, romanNumberToArabic.convertToArabic("XXXX"));
+        assertEquals(-1, romanNumberToArabic.convertToArabic("CCCC"));
+        assertEquals(-1, romanNumberToArabic.convertToArabic("CCCC"));
+    }*/
 
 }
