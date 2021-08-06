f83be957f5e6da5a866f819bbc1ecfc3fdc3e120
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-08-06 12:27:13 +0300

    BubbleSort updated #6

--- BubbleSort/src/main/java/BubbleSort.java
+++ BubbleSort/src/main/java/BubbleSort.java
@@ -1,36 +1,40 @@
 import java.util.List;
 
-public class BubbleSort{
+public class BubbleSort {
 
-    public List<Integer> sort(List<Integer> array){
-        if(array==null){return null;}
-        boolean swapped=true;
-        while(swapped){ //if no element swapped in for loop - done.
-            swapped=false;
-            for(int x=0; x<array.size()-1; x++){
-                if(array.get(x) > array.get(x+1)){
-                    int temp=array.get(x);
-                    array.set(x, array.get(x+1));
-                    array.set(x+1, temp);
-                    swapped=true;
+    public List<Integer> sort(List<Integer> array) {
+        if (array == null) {
+            return null;
+        }
+        boolean swapped = true;
+        while (swapped) { //if no element swapped in for loop - done.
+            swapped = false;
+            for (int x = 0; x < array.size() - 1; x++) {
+                if (array.get(x) > array.get(x + 1)) {
+                    int temp = array.get(x);
+                    array.set(x, array.get(x + 1));
+                    array.set(x + 1, temp);
+                    swapped = true;
                 }
             }
         }
         return array;
     }
 
-    public <T extends Number> List<T> sortGenerics(List<T> array){
-        if(array==null){return null;}
-        boolean swapped=true;
-        while(swapped){
-            swapped=false;
-            for(int x=0; x<array.size()-1; x++){
-                if(array.get(x).doubleValue() > array.get(x+1).doubleValue()){
-                    T element=array.get(x);
-                    T element2=array.get(x+1);
+    public <T extends Number> List<T> sortGenerics(List<T> array) {
+        if (array == null) {
+            return null;
+        }
+        boolean swapped = true;
+        while (swapped) {
+            swapped = false;
+            for (int x = 0; x < array.size() - 1; x++) {
+                if (array.get(x).doubleValue() > array.get(x + 1).doubleValue()) {
+                    T element = array.get(x);
+                    T element2 = array.get(x + 1);
                     array.set(x, element2);
-                    array.set(x+1, element);
-                    swapped=true;
+                    array.set(x + 1, element);
+                    swapped = true;
                 }
             }
         }
--- BubbleSort/src/test/java/BubbleSortTest.java
+++ BubbleSort/src/test/java/BubbleSortTest.java
@@ -10,18 +10,18 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BubbleSortTest {
-    private final BubbleSort bubbleSort= new BubbleSort();
+    private final BubbleSort bubbleSort = new BubbleSort();
 
     @DisplayName("sort() 5,1,4,2,8")
     @Test
     void sort() {
-        assertEquals(Arrays.asList(1,2,4,5,8), bubbleSort.sort(Arrays.asList(5,1,4,2,8)));
+        assertEquals(Arrays.asList(1, 2, 4, 5, 8), bubbleSort.sort(Arrays.asList(5, 1, 4, 2, 8)));
     }
 
     @DisplayName("sort() 1,2,3,4,5")
     @Test
     void sort_notChange() {
-        assertEquals(Arrays.asList(1,2,3,4,5), bubbleSort.sort(Arrays.asList(1,2,3,4,5)));
+        assertEquals(Arrays.asList(1, 2, 3, 4, 5), bubbleSort.sort(Arrays.asList(1, 2, 3, 4, 5)));
     }
 
     @DisplayName("sort() null")
@@ -39,20 +39,20 @@ class BubbleSortTest {
     @DisplayName("sortGenerics() with Integer")
     @Test
     void sortGenerics_integer() {
-        assertEquals(Arrays.asList(1,2,4,5,8), bubbleSort.sortGenerics(Arrays.asList(5,1,4,2,8)));
+        assertEquals(Arrays.asList(1, 2, 4, 5, 8), bubbleSort.sortGenerics(Arrays.asList(5, 1, 4, 2, 8)));
     }
 
 
     @DisplayName("sortGenerics() with Double")
     @Test
     void sortGenerics_double() {
-        assertEquals(Arrays.asList(1.1,2.1,4.2,5.5,8.8), bubbleSort.sortGenerics(Arrays.asList(1.1,5.5,8.8,4.2,2.1)));
+        assertEquals(Arrays.asList(1.1, 2.1, 4.2, 5.5, 8.8), bubbleSort.sortGenerics(Arrays.asList(1.1, 5.5, 8.8, 4.2, 2.1)));
     }
 
     @DisplayName("sortGenerics() with Float")
     @Test
     void sortGenerics_float() {
-        assertEquals(Arrays.asList(-2.1f,1.1f,4.2f,5.5f,8.8f), bubbleSort.sortGenerics(Arrays.asList(1.1f,5.5f,8.8f,4.2f,-2.1f)));
+        assertEquals(Arrays.asList(-2.1f, 1.1f, 4.2f, 5.5f, 8.8f), bubbleSort.sortGenerics(Arrays.asList(1.1f, 5.5f, 8.8f, 4.2f, -2.1f)));
     }
 
     @DisplayName("sortGenerics() null")
